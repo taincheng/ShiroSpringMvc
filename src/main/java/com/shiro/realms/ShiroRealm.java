efe8e8432b25ce6a9d9724403eb374cb6b4bef34
Old_man <402503294@qq.com>
2019-08-14 01:18:03 +0800

    使用MD5 加盐 进行加密

--- src/main/java/com/shiro/realms/ShiroRealm.java
+++ src/main/java/com/shiro/realms/ShiroRealm.java
@@ -1,9 +1,11 @@
 package com.shiro.realms;
 
 import org.apache.shiro.authc.*;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.realm.AuthenticatingRealm;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.realm.Realm;
+import org.apache.shiro.util.ByteSource;
 
 /**
  * @Author TianCheng
@@ -22,11 +24,11 @@ public class ShiroRealm extends AuthenticatingRealm {
         // 3. 调用数据库的方法来从数据库中查询username对应的用户记录
         System.out.println("从数据库中获取username ：" + username + "所对应的信息");
         // 4. 若用户不存在， 则可以抛出 UnknownAccountException 异常
-        if("unknown".equals(username)){
+        if ("unknown".equals(username)) {
             throw new UnknownAccountException("用户不存在");
         }
         // 5. 根据用户信息的情况，决定是否需要抛出其他的 AuthenticationException 异常。
-        if("monster".equals(username)){
+        if ("monster".equals(username)) {
             throw new LockedAccountException("用户被锁定");
         }
         // 6.根据用户的情况，来构建AuthenticationInfo 对象并返回.
@@ -34,11 +36,28 @@ public class ShiroRealm extends AuthenticatingRealm {
         //1. principal: 认证的实体信息，可以是username，也可以是数据表对应的用户的实体类对象。
         Object principal = username;
         //2. credentials : 密码。
-        Object credentials = "123456";
+        Object credentials = null;
+        if ("admin".equals(username)) {
+            credentials = "038bdaf98f2037b31f1e75b5b4c9b26e";
+        } else if ("user".equals(username)) {
+            credentials = "098d2c478e9c11555ce2823231e02ec1";
+        }
         //3. realmName : 当前realm对象的name，调用父类的 getName()方法即可。
         String realmName = getName();
-
-        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(principal,credentials,realmName);
+        //4. 盐值。
+        ByteSource credentialsSalt = ByteSource.Util.bytes(username);
+        SimpleAuthenticationInfo info = null;
+        info = new SimpleAuthenticationInfo(principal, credentials, credentialsSalt,realmName);
         return info;
+    }
+
+    public static void main(String[] args) {
+        String hashAlgorithmName = "MD5";
+        Object credentials = "123456";
+        Object salt = ByteSource.Util.bytes("user");
+        int hashIterations = 1024;
+
+        Object result = new SimpleHash(hashAlgorithmName, credentials, salt, hashIterations);
+        System.out.println(result);
     }
 }
